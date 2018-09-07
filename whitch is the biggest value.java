0efa3f09040d84fed3d7b40477ae49a134c71ed1
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-09-07 15:41:26 +0530

    Add files via upload

---
+++ whitch is the biggest value.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    int a,b;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the first :");
+    a=sc.nextInt();
+    System.out.println("Enter the sec :");
+    b=sc.nextInt();
+    if(a>b)
+{
+  System.out.println("first is big");
+}
+else
+{
+  System.out.println("second is big");
+}
+
+
+  }
+
+  }
