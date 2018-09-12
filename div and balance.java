d73fb66500646dd811d14371ebbe34f31c7a6e05
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-09-12 15:41:48 +0530

    Add files via upload

---
+++ div and balance.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class div and balance
+{
+  public static void main(String[] args)
+  {
+    int a,b,c,d;
+Scanner ka=new Scanner(System.in);
+System.out.println("enter the devisor value");
+a=ka.nextInt();
+System.out.println("enter the devider value");
+b=ka.nextInt();
+c=a/b;
+d=a%b;
+System.out.println("ans= "+c);
+System.out.println("balace is "+d);
+  }
+}
