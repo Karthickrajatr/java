330e2712f4b52820cb77cc4b4421399bf96631ad
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-09-15 14:42:45 +0530

    Add files via upload

---
+++ Find vowels.java
@@ -0,0 +1,13 @@
+class Find vowels{
+  public static void main(String[] args){   
+  char z='i';
+  if(z=='a'||z=='e'||z=='i'||z=='o'||z=='u')
+  {
+    System.out.println("the letter is vowels");
+  }
+  else
+  {
+    System.out.println("the letter is not vowels");
+  }
+  }
+}
