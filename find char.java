5e1a30992d29ec73225cf5fe0410d8ccdddb4ca7
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-09-17 14:59:54 +0530

    Add files via upload

---
+++ find char.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class find char{
+  public static void main(String args[]){
+    Scanner ka=new Scanner(System.in);
+    String name,city;
+    int a,b;
+    System.out.println("enter your name");
+    name=ka.next();
+    System.out.println("enter your age");
+    a=ka.nextInt();
+    System.out.println("enter your hight");
+    b=ka.nextInt();
+    System.out.println("enter your city");
+    city=ka.next();
+    System.out.println("name is"+name);
+    System.out.println("age is"+a);
+    System.out.println("hight is"+b);
+    System.out.println("city is"+city);
+  }
+}
