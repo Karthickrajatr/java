efef032600b73eaa9c888e14cdebc572cc3920c4
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-10-04 11:45:33 +0530

    Add files via upload

---
+++ Check_leap_year.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Check_leap_year
+{
+  public static void main(String[] args)
+  {
+    Scanner ka=new Scanner(System.in);
+    int a;
+    a=ka.nextInt();
+   if(a%4==0) 
+   {
+     System.out.println("Yes");
+   }
+   else
+   {
+     System.out.println("No");
+   }
+  }
+}
