bad68fffc7cda870a4358538adb76df6f278aabf
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-10-04 12:00:51 +0530

    Add files via upload

---
+++ N_times_print.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class N_times_print
+{
+  public static void main(String[] args)
+  {
+    Scanner ka=new Scanner(System.in);
+    int a,i;
+    a=ka.nextInt();
+    i=0;
+while(i<a)
+{
+  System.out.println("Hello");
+  i=i+1;
+}
+
+  }
+}
