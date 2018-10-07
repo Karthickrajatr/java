64c4f1bf5f75e95236357a2d9f6d461894c987ee
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-10-07 06:11:39 -0700

    Add files via upload

---
+++ count_number_of_digit.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class count_number_of_digit
+{
+public static void main(String[] args)
+{
+  Scanner ka=new Scanner(System.in);
+  int word=1;
+  String str;
+  str=ka.next();
+  for(int i=0;i<str.length();++i)
+  {
+   if(str.charAt(i)==' ')
+    word++;
+  }
+ 
+  System.out.println(""+(word-1));
+}
+}
