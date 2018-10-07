29e8c264e6dc325045328945bc751ce2543a1c5e
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-10-07 12:57:16 -0700

    Add files via upload

---
+++ connect_two_string.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class connect_two_string
+{ 
+  public static void main(String args[]) 
+  {
+    Scanner ka=new Scanner(System.in);
+     String firstname ,lastname; 
+     firstname=ka.next();
+     lastname=ka.next();
+     String name = firstname + lastname; 
+     System.out.println(name);
+  }
+}
+
+
