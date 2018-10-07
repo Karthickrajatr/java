b9e2842dff8d1fa08cb0b766801870a035ae2ef6
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-10-07 05:55:24 -0700

    Add files via upload

---
+++ Array_n_integer.java
@@ -0,0 +1,16 @@
+import java.util.*;
+  class Array_n_integer
+{
+ 
+    static int largest(int []arr, int n)
+    {
+        Arrays.sort(arr);
+        return arr[n - 1];
+    }
+    static public void main (String[] args)
+    {
+        int []arr = {10, 324, 45, 90, 9808};
+        int n = arr.length;
+        System.out.println(largest(arr, n));
+    }
+} 
