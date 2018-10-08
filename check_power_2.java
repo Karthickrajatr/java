072243e5e0adcc3e9dac04954acaac63499c39d5
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-10-07 18:04:42 -0700

    Add files via upload

---
+++ check_power_2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class check_power_2
+{ 
+  public static void main(String args[]) 
+  {
+    Scanner ka=new Scanner(System.in);
+     int n;
+     n=ka.nextInt();
+   if(n%2==0)
+   {
+     System.out.println("yes");
+   }
+   else
+   {
+     System.out.println("no");
+   }
+   }
+  }
+
+
