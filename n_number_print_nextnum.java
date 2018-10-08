0021ee7b1523c5f507186d30151510047f55e4a0
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-10-07 17:14:03 -0700

    Add files via upload

---
+++ n_number_print_nextnum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class n_number_print_nextnum
+{ 
+  public static void main(String args[]) 
+  {
+    Scanner ka=new Scanner(System.in);
+     int n,a;
+     n=ka.nextInt();
+    a=n+1;
+    System.out.println(""+a);
+  }
+}
+
