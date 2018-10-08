29fc756bf33d9a4c3d3c956576a771d2f126ea4c
Karthickrajatr <43032322+Karthickrajatr@users.noreply.github.com>
2018-10-07 17:58:48 -0700

    Add files via upload

---
+++ sum_of_2num.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{ 
+  public static void main(String args[]) 
+  {
+    Scanner ka=new Scanner(System.in);
+     int n,b,c;
+     n=ka.nextInt();
+   c=ka.nextInt();
+   b=n+c;
+   System.out.println(""+b);
+   }
+  }
+
+
